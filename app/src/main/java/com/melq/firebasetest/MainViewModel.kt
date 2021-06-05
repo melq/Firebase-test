0af23192e5c1c5af951bea22bfe9b88b18f98666
melq <tu2sice@yahoo.co.jp>
2021-06-05 15:41:58 +0900

    :recycle: ViewModelのプロパティ定義を修正

--- app/src/main/java/com/melq/firebasetest/MainViewModel.kt
+++ app/src/main/java/com/melq/firebasetest/MainViewModel.kt
@@ -6,15 +6,12 @@ import androidx.lifecycle.ViewModel
 import kotlin.random.Random
 
 class MainViewModel : ViewModel() {
-    private val _text: MutableLiveData<String> =
-        MutableLiveData<String>().also { mutableLiveData ->
-            mutableLiveData.value = "main"
-        }
+    private val _text: MutableLiveData<String> by lazy { MutableLiveData<String>("main") }
     val text: LiveData<String> get() = _text
 
-    private val textList: MutableList<String> = mutableListOf<String>("alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel")
+    private val textList: MutableList<String> = mutableListOf("alpha", "bravo", "charlie", "delta", "echo", "foxtrot", "golf", "hotel")
 
     fun changeText() {
-        _text.value = textList[Random.nextInt(8)]
+        _text.value = textList[Random.nextInt(textList.size)]
     }
 }
