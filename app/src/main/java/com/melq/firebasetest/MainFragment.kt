8455170d5c08a6a2a15b74efa036395daf501762
melq <tu2sice@yahoo.co.jp>
2021-06-05 12:49:33 +0900

    :star: Fragmentを作成し、Navigationに追加

---
+++ app/src/main/java/com/melq/firebasetest/MainFragment.kt
@@ -0,0 +1,7 @@
+package com.melq.firebasetest
+
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+}
---
+++ app/src/main/java/com/melq/firebasetest/page/login/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.melq.firebasetest.page.login
+
+import androidx.fragment.app.Fragment
+import com.melq.firebasetest.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+}
