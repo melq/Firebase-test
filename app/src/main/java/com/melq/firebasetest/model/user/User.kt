5d52b466bdb6d341ec5eac48e18264e038b29512
melq <tu2sice@yahoo.co.jp>
2021-06-07 23:03:12 +0900

    :star: Userクラスの作成

---
+++ app/src/main/java/com/melq/firebasetest/model/user/User.kt
@@ -0,0 +1,11 @@
+package com.melq.firebasetest.model.user
+
+class User (val first: String, val last: String, val born: Int) {
+    fun toHashMap() : HashMap<String, java.io.Serializable> {
+        return hashMapOf(
+            "first" to this.first,
+            "last" to this.last,
+            "born" to this.born
+        )
+    }
+}
