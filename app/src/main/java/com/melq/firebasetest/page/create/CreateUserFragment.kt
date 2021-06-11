dca4c16f40cf22f93eddfb27ed1189634b80640b
melq <tu2sice@yahoo.co.jp>
2021-06-11 21:10:36 +0900

    :sparkles: ユーザ作成画面の機能を実装した

--- app/src/main/java/com/melq/firebasetest/page/create/CreateUserFragment.kt
+++ app/src/main/java/com/melq/firebasetest/page/create/CreateUserFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import com.melq.firebasetest.R
 import com.melq.firebasetest.databinding.FragmentCreateUserBinding
 
@@ -16,6 +18,27 @@ class CreateUserFragment : Fragment(R.layout.fragment_create_user) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         this._binding = FragmentCreateUserBinding.bind(view)
+
+        binding.tvOk.setOnClickListener {
+            vm.createUser(
+                binding.etUserName.text.toString(),
+                binding.etFirstName.text.toString(),
+                binding.etLastName.text.toString(),
+                binding.etBorn.text.toString()
+            )
+            vm.errorMessage.observe(viewLifecycleOwner) { msg ->
+                if (msg.isEmpty()) return@observe
+                Snackbar.make(requireView(), msg, Snackbar.LENGTH_SHORT).show()
+                vm.errorMessage.value = ""
+            }
+            vm.done.observe(viewLifecycleOwner) {
+                if (it == true) findNavController().popBackStack()
+            }
+        }
+
+        binding.tvCancel.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/melq/firebasetest/page/create/CreateUserViewModel.kt
+++ app/src/main/java/com/melq/firebasetest/page/create/CreateUserViewModel.kt
@@ -1,6 +1,30 @@
 package com.melq.firebasetest.page.create
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.melq.firebasetest.model.user.User
+import com.melq.firebasetest.model.user.UserFireStore
+import kotlinx.coroutines.launch
 
 class CreateUserViewModel : ViewModel() {
+    val errorMessage = MutableLiveData<String>()
+    val done = MutableLiveData<Boolean>()
+
+    fun createUser(id: String, first: String, last: String, born: String) {
+        if (id.isNotEmpty() &&
+            first.isNotEmpty() &&
+            last.isNotEmpty()&&
+            born.isNotEmpty()
+        ) {
+            viewModelScope.launch {
+                UserFireStore().createUser(User(id, first, last, born.toInt()))
+                done.value = true
+            }
+        } else {
+            errorMessage.value = "Please input all"
+            return
+        }
+
+    }
 }
