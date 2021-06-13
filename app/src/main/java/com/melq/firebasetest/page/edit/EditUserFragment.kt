aad7690ef8febde2ccaa280e04212e38a3fa0a7f
melq <tu2sice@yahoo.co.jp>
2021-06-13 15:17:30 +0900

    :construction: Edit画面にDataBindingを導入

--- app/src/main/java/com/melq/firebasetest/page/edit/EditUserFragment.kt
+++ app/src/main/java/com/melq/firebasetest/page/edit/EditUserFragment.kt
@@ -1,26 +1,31 @@
 package com.melq.firebasetest.page.edit
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.melq.firebasetest.R
 import com.melq.firebasetest.databinding.FragmentEditUserBinding
 
 class EditUserFragment : Fragment(R.layout.fragment_edit_user) {
+    private lateinit var binding: FragmentEditUserBinding
     private val vm: EditUserViewModel by viewModels()
 
-    private var _binding: FragmentEditUserBinding? = null
-    private val binding: FragmentEditUserBinding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentEditUserBinding.inflate(inflater, container, false).also {
+            it.vm = vm
+            it.lifecycleOwner = this
+        }
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        _binding = FragmentEditUserBinding.bind(view)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
     }
 }
--- app/src/main/java/com/melq/firebasetest/page/edit/EditUserViewModel.kt
+++ app/src/main/java/com/melq/firebasetest/page/edit/EditUserViewModel.kt
@@ -1,8 +1,32 @@
 package com.melq.firebasetest.page.edit
 
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.melq.firebasetest.model.user.User
 
 class EditUserViewModel : ViewModel() {
     lateinit var user: User
+
+    private val _userName = MutableLiveData<String>()
+    val userName: LiveData<String> get() = _userName
+
+    private val _firstName = MutableLiveData<String>()
+    val firstName: LiveData<String> get() = _firstName
+
+    private val _lastName = MutableLiveData<String>()
+    val lastName: LiveData<String> get() = _lastName
+
+    private val _born = MutableLiveData<Int>()
+    val born: LiveData<Int> get() = _born
+
+    fun putUserData(user: User) {
+        this.user = user.apply {
+            _userName.value = id
+            _firstName.value = first
+            _lastName.value = last
+            _born.value = born
+        }
+    }
 }
