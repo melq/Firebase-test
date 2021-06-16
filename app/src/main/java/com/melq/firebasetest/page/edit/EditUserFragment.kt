889f64c7ffb53c88efdbfcf389f9d3f6d121eb41
melq <tu2sice@yahoo.co.jp>
2021-06-16 20:10:35 +0900

    :construction: 編集画面のキャンセルボタンを実装

--- app/src/main/java/com/melq/firebasetest/page/edit/EditUserFragment.kt
+++ app/src/main/java/com/melq/firebasetest/page/edit/EditUserFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.melq.firebasetest.ActivityViewModel
 import com.melq.firebasetest.R
 import com.melq.firebasetest.databinding.FragmentEditUserBinding
@@ -28,5 +29,9 @@ class EditUserFragment : Fragment(R.layout.fragment_edit_user) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tvCancel.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
