23ddcfe22970251c5fbe63e228b8c5c442a3038b
melq <tu2sice@yahoo.co.jp>
2021-06-24 16:19:49 +0900

    :art: Login画面の見た目を作成

--- app/src/main/java/com/melq/firebasetest/page/login/LoginFragment.kt
+++ app/src/main/java/com/melq/firebasetest/page/login/LoginFragment.kt
@@ -1,7 +1,21 @@
 package com.melq.firebasetest.page.login
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.melq.firebasetest.ActivityViewModel
 import com.melq.firebasetest.R
+import com.melq.firebasetest.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
+    val vm: ActivityViewModel by activityViewModels()
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding: FragmentLoginBinding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentLoginBinding.bind(view)
+    }
 }
